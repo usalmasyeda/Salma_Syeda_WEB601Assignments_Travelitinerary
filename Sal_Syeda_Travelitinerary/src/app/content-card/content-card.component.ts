4314806a72c8278abb1d52d11a60196f314adf57
Umme Salma Syeda <W0830190@myscc.ca>
2024-01-20 18:02:12 -0500

    Created Content Card Folder

---
+++ Sal_Syeda_Travelitinerary/src/app/content-card/content-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-card',
+  templateUrl: './content-card.component.html',
+  styleUrl: './content-card.component.scss'
+})
+export class ContentCardComponent {
+
+}

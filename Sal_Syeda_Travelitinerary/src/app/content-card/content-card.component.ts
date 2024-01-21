2fc39ff1485f86603eab45be95d8ce4e7951b47b
Umme Salma Syeda <W0830190@myscc.ca>
2024-01-20 19:04:09 -0500

    Changed img url

--- Sal_Syeda_Travelitinerary/src/app/content-card/content-card.component.ts
+++ Sal_Syeda_Travelitinerary/src/app/content-card/content-card.component.ts
@@ -15,7 +15,7 @@ export class ContentCardComponent {
       title: 'Paris Adventure',
       description: 'Explore the romantic city of Paris, known for its iconic landmarks such as the Eiffel Tower and Louvre Museum.',
       creator: 'Sal Syeda',
-      imgUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftravel.usnews.com%2FParis_France%2FThings_To_Do%2F&psig=AOvVaw2nsoByZqNKOa-VRahp7zeE&ust=1705878639832000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOiH6KyL7YMDFQAAAAAdAAAAABAE',
+      imgUrl: 'https://media.cntraveler.com/photos/5d8cf7d5db6acf000833e6cc/master/pass/Eiffel-Tower_GettyImages-1060266626.jpg',
       type: 'Proposal',
       tags: ['Paris', 'travel']
     });
@@ -25,7 +25,7 @@ export class ContentCardComponent {
       title: 'Tokyo Escapade',
       description: 'Experience the vibrant culture of Tokyo, from traditional temples to modern tech districts like Akihabara.',
       creator: 'Sal Syeda',
-      imgUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ffacts.net%2Fworld%2Fcities%2F45-facts-about-tokyo%2F&psig=AOvVaw1XwEqehyJtajrB91LChFWU&ust=1705878709844000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLjC3MeL7YMDFQAAAAAdAAAAABAD',
+      imgUrl: 'https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg',
       type: 'Anime',
       tags: ['Tokyo', 'travel']
     });
@@ -35,7 +35,7 @@ export class ContentCardComponent {
       title: 'New York City Exploration',
       description: 'Discover the city that never sleeps, with its iconic skyline, Broadway shows, and Central Park.',
       creator: 'Sal Syeda',
-      imgUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.livescience.com%2Fplanet-earth%2Fclimate-change%2Fnew-york-city-may-be-sinking-under-its-own-weight-because-the-buildings-are-too-heavy-scientists-warn&psig=AOvVaw1lCeTGfFG8IK4Qp9gkvyTH&ust=1705878745181000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOiN2dqL7YMDFQAAAAAdAAAAABAD',
+      imgUrl: 'https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg',
       type: 'Fashion',
       tags: ['New York', 'travel']
     });
@@ -45,7 +45,7 @@ export class ContentCardComponent {
       title: 'Santorini Getaway',
       description: 'Escape to the beautiful Greek island of Santorini, known for its stunning sunsets and white-washed buildings.',
       creator: 'Sal Syeda',
-      imgUrl: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.moon.com%2Ftravel%2Fplanning%2Fa-weekend-in-santorini%2F&psig=AOvVaw1KFOl9Az8Rz94IDyqo60kl&ust=1705878776381000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCgteqL7YMDFQAAAAAdAAAAABAI',
+      imgUrl: 'https://images.rove.me/santorini_w1280.jpg',
       type: 'Vacation',
       tags: ['Santorini', 'travel']
     });
@@ -55,7 +55,7 @@ export class ContentCardComponent {
       title: 'Rome Adventure',
       description: 'Immerse yourself in the rich history of Rome, with its ancient ruins, Vatican City, and delicious Italian cuisine.',
       creator: 'Sal Syeda',
-      imgUrl: 'https://ca.hotels.com/go/italy/colosseum-rome?intlid=gglist|listitem:image',
+      imgUrl: 'https://a.cdn-hotels.com/gdcs/production151/d623/a8b25d0d-64d5-402f-99f5-a58f483c38c6.jpg?impolicy=fcrop&w=800&h=533&q=medium',
       type: 'History',
       tags: ['Rome', 'travel']
     });

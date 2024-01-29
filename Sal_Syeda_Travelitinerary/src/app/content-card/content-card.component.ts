5e03b8849a2b174013a4b156a9179927e11a25ce
Umme Salma Syeda <W0830190@myscc.ca>
2024-01-28 19:25:51 -0500

    Content-list scss

--- Sal_Syeda_Travelitinerary/src/app/content-card/content-card.component.ts
+++ Sal_Syeda_Travelitinerary/src/app/content-card/content-card.component.ts
@@ -1,67 +1,18 @@
-import { Component } from '@angular/core';
-// import { ContentList } from '../helper-files/content-list';
+// content-card.component.ts
+import { Component, Input } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
-  styleUrl: './content-card.component.scss'
+  styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent {
-  // contentList: ContentList;
+  @Input() content!: Content;
 
-  constructor() {
-//     this.contentList = new ContentList({
-//       id: 1,
-//       title: 'Paris Adventure',
-//       description: 'Explore the romantic city of Paris, known for its iconic landmarks such as the Eiffel Tower and Louvre Museum.',
-//       creator: 'Sal Syeda',
-//       imgUrl: 'https://media.cntraveler.com/photos/5d8cf7d5db6acf000833e6cc/master/pass/Eiffel-Tower_GettyImages-1060266626.jpg',
-//       type: 'Proposal',
-//       tags: ['Paris', 'travel']
-//     });
+  constructor() {}
 
-//     this.contentList.add({
-//       id: 2,
-//       title: 'Tokyo Escapade',
-//       description: 'Experience the vibrant culture of Tokyo, from traditional temples to modern tech districts like Akihabara.',
-//       creator: 'Sal Syeda',
-//       imgUrl: 'https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg',
-//       type: 'Anime',
-//       tags: ['Tokyo', 'travel']
-//     });
-
-//     this.contentList.add({
-//       id: 3,
-//       title: 'New York City Exploration',
-//       description: 'Discover the city that never sleeps, with its iconic skyline, Broadway shows, and Central Park.',
-//       creator: 'Sal Syeda',
-//       imgUrl: 'https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg',
-//       type: 'Fashion',
-//       tags: ['New York', 'travel']
-//     });
-
-//     this.contentList.add({
-//       id: 4,
-//       title: 'Santorini Getaway',
-//       description: 'Escape to the beautiful Greek island of Santorini, known for its stunning sunsets and white-washed buildings.',
-//       creator: 'Sal Syeda',
-//       imgUrl: 'https://images.rove.me/santorini_w1280.jpg',
-//       type: 'Vacation',
-//       tags: ['Santorini', 'travel']
-//     });
-
-//     this.contentList.add({
-//       id: 5,
-//       title: 'Rome Adventure',
-//       description: 'Immerse yourself in the rich history of Rome, with its ancient ruins, Vatican City, and delicious Italian cuisine.',
-//       creator: 'Sal Syeda',
-//       imgUrl: 'https://a.cdn-hotels.com/gdcs/production151/d623/a8b25d0d-64d5-402f-99f5-a58f483c38c6.jpg?impolicy=fcrop&w=800&h=533&q=medium',
-//       type: 'History',
-//       tags: ['Rome', 'travel']
-//     });
-//   }
-//   ngOnInit(): void {
-//     let invalidIndexContent = this.contentList.displayItem(6);
-//     console.log(invalidIndexContent);
-}
+  displayContentInfo() {
+    console.log(`Clicked on Content ID: ${this.content.id}, Title: ${this.content.title}`);
+  }
 }
--- Sal_Syeda_Travelitinerary/src/app/content-list/content-list.component.ts
+++ Sal_Syeda_Travelitinerary/src/app/content-list/content-list.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
   styleUrl: './content-list.component.scss'
 })
-export class ContentListComponent {
+export class ContentListComponent implements OnInit{
+  contentList: Content[] = [
+  {
+
+    id: 1,
+    title: 'Paris Adventure',
+    description: 'Explore the romantic city of Paris, known for its iconic landmarks such as the Eiffel Tower and Louvre Museum.',
+    creator: 'Sal Syeda',
+    imgUrl: 'https://media.cntraveler.com/photos/5d8cf7d5db6acf000833e6cc/master/pass/Eiffel-Tower_GettyImages-1060266626.jpg',
+    type: 'Proposal',
+    tags: ['Paris', 'travel']
+
+  },
+  {
+    id: 2,
+    title: 'Tokyo Escapade',
+    description: 'Experience the vibrant culture of Tokyo, from traditional temples to modern tech districts like Akihabara.',
+    creator: 'Sal Syeda',
+    imgUrl: 'https://hips.hearstapps.com/hmg-prod/images/high-angle-view-of-tokyo-skyline-at-dusk-japan-royalty-free-image-1664309926.jpg',
+    type: 'Anime',
+    tags: ['Tokyo', 'travel']
+  },
+  {
+    id: 3,
+    title: 'New York City Exploration',
+    description: 'Discover the city that never sleeps, with its iconic skyline, Broadway shows, and Central Park.',
+    creator: 'Sal Syeda',
+    imgUrl: 'https://www.travelandleisure.com/thmb/91pb8LbDAUwUN_11wATYjx5oF8Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg',
+    type: 'Fashion',
+    tags: ['New York', 'travel']
+  },
+  {
+    id: 4,
+    title: 'Santorini Getaway',
+    description: 'Escape to the beautiful Greek island of Santorini, known for its stunning sunsets and white-washed buildings.',
+    creator: 'Sal Syeda',
+    imgUrl: 'https://images.rove.me/santorini_w1280.jpg',
+    type: 'Vacation',
+    tags: ['Santorini', 'travel']
+  },
+  {
+    id: 5,
+    title: 'Rome Adventure',
+    description: 'Immerse yourself in the rich history of Rome, with its ancient ruins, Vatican City, and delicious Italian cuisine.',
+    creator: 'Sal Syeda',
+    imgUrl: '',
+    type: '', // Bonus
+    tags: []
+  }
+];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
